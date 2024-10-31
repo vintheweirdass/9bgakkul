82441bfd974b18fbfd60adbbc72ba66ef9a31c0a
vincmatheo <vincmatheo@gmail.com>
2024-10-31 07:00:54 +0000

    Docking issue solved

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,7 +1,7 @@
 import {Button} from "@mui/material"
 import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faInstagram} from "@fortawesome/free-brands-svg-icons"
+import {faInstagram, faSpotify} from "@fortawesome/free-brands-svg-icons"
 import {faHome, faUsers} from "@fortawesome/free-solid-svg-icons";
 import { CSSProperties } from "react";
 const dockStyle: CSSProperties = {
@@ -9,6 +9,12 @@ const dockStyle: CSSProperties = {
   flexDirection: "row", // Arrange items in a row
   justifyContent: "space-between", // Distribute space between items
   alignItems: "center", // Vertically align items to the center
+  position:"fixed",
+    top:0,
+    left:1,
+    width:"100%",
+    zIndex:100,
+    backgroundColor:"black"
 };
 
 const btnStyle:CSSProperties = {
@@ -17,7 +23,7 @@ const btnStyle:CSSProperties = {
   border:"none"
 }
 export default function () {
-    return <div style={dockStyle}>
+    return <><div style={dockStyle}>
     <div>
     <Link to="/">
     <Button style={btnStyle}><FontAwesomeIcon icon={faHome} fontSize={30} color="white"/></Button>
@@ -30,6 +36,15 @@ export default function () {
     <Link to="https://instagram.com/9b_iscooked">
     <Button style={btnStyle}><FontAwesomeIcon icon={faInstagram} fontSize={30} color="white"/></Button>
     </Link>
+    <Link to="https://open.spotify.com/playlist/1DHqRh4zXuziQCKTL9x6tb">
+    <Button style={btnStyle}><FontAwesomeIcon icon={faSpotify} fontSize={30} color="white"/></Button>
+    </Link>
     </div>
     </div>
+    <div style={
+      {
+        marginBottom:100
+      }
+    }></div>
+    </>
  }
