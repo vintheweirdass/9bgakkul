aa895bfd454f29b888a38e44115eb80976b9725d
vincmatheo <vincmatheo@gmail.com>
2024-11-01 05:39:35 +0000

    nambahin temen (belom selese)

--- src/Friends.tsx
+++ src/Friends.tsx
@@ -1,15 +1,17 @@
-import { Avatar, Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputBase, alpha, styled, useMediaQuery, useTheme } from "@mui/material"
-import Dock from "./components/Dock"
+import { Avatar, Button, Card, CardContent, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Grid2, InputBase, Stack, Typography, alpha, styled, useMediaQuery, useTheme } from "@mui/material";
+import Dock from "./components/Dock";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import React from "react";
-import { badges } from "./friendsList";
+import { Badges, badges, badgesColor, friends } from "./friendsList";
+
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: 30,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  '&:hover':
+ {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
@@ -38,7 +40,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
+    [
+theme.breakpoints.up('sm')]: {
       width: '12ch',
       '&:focus': {
         width: '20ch',
@@ -47,29 +50,94 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-
-function AlertDialog() {
- 
-  function ListofBadges(){
-    const ok = badges
-    ok.map()
-  }
+function SearchUi() {
   const [open, setOpen] = React.useState(false);
+  const [filter, setFilter] = React.useState<Badges[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleClickOpen = () => {
     setOpen(true);
   };
-  
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  function ListofBadges() {
+    const ok: JSX.Element[] = [];
+    for (const each of badges) {
+      ok.push(<Chip key={each} component="div" sx={{
+        backgroundColor: badgesColor[each],
+      }} label={each} variant="filled" onClick={() => {
+        setOpen(false);
+      }} />);
+    }
+    return ok;
+  }
+
+  function ListofFriends() {
+    const elems: JSX.Element[] = [];
+    for (const [name, info] of Object.entries(friends)) {
+      function BadgesAvailable() {
+        const ok: JSX.Element[] = [];
+        if (info.badges) {
+          for (const each of info.badges) {
+            ok.push(<Chip key={each} component="div" sx={{
+              backgroundColor: badgesColor[each as Badges],
+            }} label={each} variant="filled" />);
+          }
+        }
+        return ok;
+      }
+
+      elems.push(<Grid2 key={name} size={7}>
+        <Card sx={{width:265}}> 
+        <CardContent>
+          <BadgesAvailable />
+          <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+            Badges
+          </Typography>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" sx={{color:"text.secondary"}}>
+            {info.description}
+          </Typography>
+        <Button size="large">Learn More</Button>
+        </CardContent>
+        </Card>
+</Grid2>);
+    }
+    return <Grid2 container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+  alignItems="center">
+      {elems}
+    </Grid2>;
+  }
+
   return (
     <React.Fragment>
-      
-    <Chip sx={{
-      marginBottom:3,
-      fontSize:18
-    }} avatar={<Avatar>14</Avatar>} label="Show all available badges" onClick={handleClickOpen}/>
+      <Search>
+      <SearchIconWrapper>
+        <FontAwesomeIcon icon={faSearch} />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            const src = searchParams
+            src.set("search", (e.target as HTMLInputElement).value);
+            setSearchParams(src)
+            e.preventDefault();
+          }
+        }}
+      />
+    </Search>
+      <Chip sx={{
+        margin: 3,
+        fontSize: 18
+      }} avatar={<Avatar>{badges.length}</Avatar>} label="Show all
+ available badges" onClick={handleClickOpen} />
       <Dialog
         open={open}
         onClose={handleClose}
@@ -80,9 +148,15 @@ function AlertDialog() {
           {"Select your badges here"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            <ListofBadges/>
-          </DialogContentText>
+          <Stack
+            spacing={{ xs: 1, sm: 2 }}
+            direction="row"
+            useFlexGap
+            sx={{ flexWrap: 'wrap' }}
+          >
+            
+<ListofBadges />
+          </Stack>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} autoFocus>
@@ -90,24 +164,18 @@ function AlertDialog() {
           </Button>
         </DialogActions>
       </Dialog>
+      <ListofFriends />
     </React.Fragment>
   );
 }
-export default function(){
-    return <>
-    <Dock/>
+
+export default function () {
+  return <>
+    <Dock />
     <h1>9b friends</h1>
     <p>This is the list of friends in <code>9b</code> that already opt-in to show theirself in the friends list</p>
     <p>Or opt-out if they use the sub-sub domain feature. To prevent malicious activites to the <Link to="/">subdomain</Link>, and the <Link to="https://is-not.cool">root domain itself</Link></p>
-    <AlertDialog/>
-    <Search>
-            <SearchIconWrapper>
-              <FontAwesomeIcon icon={faSearch} />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-    </>
+    
+    <SearchUi />
+  </>;
 }
--- src/friendsList.ts
+++ src/friendsList.ts
@@ -1,22 +1,56 @@
-const tuple = <T extends string[]>(...args: T) => args;
-export const badges = tuple('Owner', 'External (not from our school)', 'is-not.cool Admin', 'Chief of Class', 'Vice of Class');
+biarin error, syntax error. Gw ntaran publish ke vercelnya
+import arka from "./assets/friendsPfp/instagram/arkanantalangit.jpg"
+import { red, orange, lightGreen, pink, lightBlue, blue } from '@mui/material/colors';
+const tuple:<T extends string[]>(...args: T) => typeof args = <T extends string[]>(...args: T) => args;
+export const badges = tuple('Owner', 'Chief of Class', 'Vice of Class', 'Normal student', 'is-not.cool Admin', 'External (not from our school)');
 export type Badges = typeof badges[number];
 export type BadgesColor = Record<Badges, string>
 export const badgesColor:BadgesColor = {
-  "Owner":"red",
-  "External (not from our school)":"orange",
-  "Chief of Class":"lightgreen",
-  "Vice of Class":"pink",
-  "is-not.cool Admin":"skyblue"
+  "Owner":red[800],
+  "External (not from our school)":orange[800],
+  "Chief of Class":lightGreen[800],
+  "Vice of Class":pink[800],
+  "is-not.cool Admin":lightBlue[800],
+  "Normal student":blue[800]
 }
 export type FriendsList = Record<string, {
-    badges: Badges|undefined;
-    avatar:string;
+    badges: Badges[];
+    avatar?:string;
+    social?:{
+      discord?:string;
+      instagram?:string;
+      spotify?:string;
+      website?:string;
+    }
+    ownsDomain?:string[];
+    description?:string;
 }>;
 
 export const friends:FriendsList =  {
     cupglassdev:{
-      badges:"Owner",
-      avatar:"https://avatars.githubusercontent.com/u/70825723?v=4"
+      badges:["Owner", "Normal student"],
+      avatar:"https://avatars.githubusercontent.com/u/70825723?v=4",
+      social:{discord:"999863217557880842"},
+      ownsDomain:["@"],
+      description:"A dumbass person who likes to destroy a Pi"
+    },
+    "arkananta.langit":{
+      badges:["Chief of Class"],
+      description:"A great captain. Supports palestine ftw",
+      avatar:arka
+    },
+    "hroiene_":{
+      badges:["Normal student"],
+      description:"just a chill guy tbh",
+      social:{
+        instagram:"hroiene_"
+      }
+    },
+    p_t_r_i_t_n_d_i_l_i_a:{
+      badges:["Normal student"],
+      social:{
+        instagram:"p_t_r_i_t_n_d_i_l_i_a"
+      },
+      description:"just me, being me."
     }
 }
