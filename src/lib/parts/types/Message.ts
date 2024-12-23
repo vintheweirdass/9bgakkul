42810b2586ad92e18a5db79c9b98a22e7b80cba3
cupglassDEV <vincmatheo@gmail.com>
2024-12-23 20:20:16 +0700

    make the error message more verbose

--- src/lib/parts/types/Message.ts
+++ src/lib/parts/types/Message.ts
@@ -4,9 +4,9 @@ export const spotifySongRegexHtml = "^(http(s)?:\\/\\/open.spotify.com\\/track\\
 export const spotifySongRegex = new RegExp(spotifySongRegexHtml)
 // svelte-ignore state_referenced_locally
 export const OptionSchema = v.objectAsync({
-    name:v.pipeAsync(v.string(), v.minLength(1, "The name's length need at least 1"), v.maxLength(50, "The name's length has exceeded more than 50")),
+    name:v.pipeAsync(v.string('The name must be defined'), v.minLength(1, "The name's length need at least 1"), v.maxLength(50, "The name's length has exceeded more than 50")),
     spotifySong:v.undefinedableAsync(v.pipeAsync(v.string(), v.regex(spotifySongRegex, 'The spotify track/song link is badly formatted'))),
-    description:v.pipeAsync(v.string(), v.minLength(1, "The description's length need at least 1"), v.maxLength(1500, "The description's length has exceeded more than 1500"))
+    description:v.pipeAsync(v.string('The description must be defined'), v.minLength(1, "The description's length need at least 1"), v.maxLength(1500, "The description's length has exceeded more than 1500"))
 })
 
 // internal option (only used on .svelte files)
